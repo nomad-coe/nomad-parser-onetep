047813f477747befc34e3affb50b43e605a0ef86
martina stella <martinastella@martinas-air.home>
2016-07-06 12:39:03 +0100

    fixing scala

--- src/test/scala/eu/nomad_lab/parsers/OnetepParserSpec.scala
+++ src/test/scala/eu/nomad_lab/parsers/OnetepParserSpec.scala
@@ -5,10 +5,10 @@ import org.specs2.mutable.Specification
 object OnetepParserSpec extends Specification {
   "OnetepParserTest" >> {
     "test with json-events" >> {
-      ParserRun.parse(OnetepParser, "parsers/onetep/test/examples/single_point.out", "json-events") must_== ParseResult.ParseSuccess
+      ParserRun.parse(OnetepParser, "parsers/onetep/test/examples/benchmark.out", "json-events") must_== ParseResult.ParseSuccess
     }
     "test with json" >> {
-      ParserRun.parse(OnetepParser, "parsers/onetep/test/examples/single_point.out", "json") must_== ParseResult.ParseSuccess
+      ParserRun.parse(OnetepParser, "parsers/onetep/test/examples/benchmark.out", "json") must_== ParseResult.ParseSuccess
     }
   }
 }
