342c83c8a935d02ff74e4479df05eac5562cf237
Markus Scheidgen <markus.scheidgen@gmail.com>
2021-01-20 13:55:17 +0100

    Prepared license and copyright headers.

--- README.md
+++ README.md
@@ -62,7 +62,15 @@ python_dict = section_run.m_to_dict()
 
 ## Developing the parser
 
-Also install NOMAD's pypi package:
+Create a virtual environment to install the parser in development mode:
+
+```
+pip install virtualenv
+virtualenv -p `which python3` .pyenv
+source .pyenv/bin/activate
+```
+
+Install NOMAD's pypi package:
 
 ```
 pip install nomad-lab
@@ -71,10 +79,8 @@ pip install nomad-lab
 Clone the parser project and install it in development mode:
 
 ```
-git clone https://gitlab.mpcdf.mpg.de/nomad-lab/parser-onetep parser-onetep
-pip install -e parser-onetep
+git clone https://github.com/nomad-coe/nomad-parser-onetep.git nomad-parser-onetep
+pip install -e nomad-parser-onetep
 ```
 
 Running the parser now, will use the parser's Python code from the clone project.
-
-
--- test/examples/TS/reac.ts
+++ test/examples/TS/reac.ts
@@ -1,3 +1,21 @@
+/*
+ * Copyright The NOMAD Authors.
+ *
+ * This file is part of NOMAD.
+ * See https://nomad-lab.eu for further info.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
  LST    0      0.00000000E+000
               -4.68109435E+001     -4.68109435E+001                       <-- E
                4.00000000E+001      0.00000000E+000      0.00000000E+000  <-- h
