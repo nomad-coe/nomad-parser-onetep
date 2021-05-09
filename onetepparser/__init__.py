22c5bfdb3cab1a414b1365b1469bee5e17493a65
Alvin Noe Ladines <ladinesalvinnoe@gmail.com>
2021-05-09 15:43:36 +0200

    Implemented new basic parser

--- onetepparser/__init__.py
+++ onetepparser/__init__.py
@@ -16,4 +16,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-from onetepparser.OnetepParser import OnetepParser
+from .onetep_parser import OnetepParser
---
+++ tests/test_onetepparser.py
@@ -0,0 +1,59 @@
+#
+# Copyright The NOMAD Authors.
+#
+# This file is part of NOMAD. See https://nomad-lab.eu for further info.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+import pytest
+
+from nomad.datamodel import EntryArchive
+from onetepparser import OnetepParser
+
+
+def approx(value, abs=0, rel=1e-6):
+    return pytest.approx(value, abs=abs, rel=rel)
+
+
+@pytest.fixture(scope='module')
+def parser():
+    return OnetepParser()
+
+
+def test_basic(parser):
+    archive = EntryArchive()
+
+    parser.parse('tests/data/test05/test05.out', archive, None)
+
+    sec_run = archive.section_run[0]
+    assert sec_run.program_version == '4.4.1'
+
+    sec_system = archive.section_run[0].section_system[0]
+    assert sec_system.lattice_vectors[2][2].magnitude == approx(2.0108734e-09)
+    assert sec_system.atom_labels[3] == 'Cl'
+    assert sec_system.atom_positions[4][2].magnitude == approx(1.01078267e-09)
+
+    sec_scc = sec_run.section_single_configuration_calculation[0]
+    assert sec_scc.energy_total.magnitude == approx(-2.24380887e-16)
+
+
+def test_1(parser):
+    archive = EntryArchive()
+
+    parser.parse('tests/data/test08/ethene_relax.out', archive, None)
+
+    sec_sccs = archive.section_run[0].section_single_configuration_calculation
+    assert len(sec_sccs) == 33
+    assert sec_sccs[8].energy_total.magnitude == approx(-5.95750082e-17)
+    assert sec_sccs[2].atom_forces[5][1].magnitude == approx(-6.68217323e-10)
