e8f694f14e2cc4f4ff8078fa04996cb28eacbd43
Alvin Noe Ladines <ladinesalvinnoe@gmail.com>
2021-08-12 21:50:12 +0200

    Adapted major changes in metainfo

--- tests/test_onetepparser.py
+++ tests/test_onetepparser.py
@@ -36,16 +36,16 @@ def test_basic(parser):
 
     parser.parse('tests/data/test05/test05.out', archive, None)
 
-    sec_run = archive.section_run[0]
-    assert sec_run.program_version == '4.4.1'
+    sec_run = archive.run[0]
+    assert sec_run.program.version == '4.4.1'
 
-    sec_system = archive.section_run[0].section_system[0]
-    assert sec_system.lattice_vectors[2][2].magnitude == approx(2.0108734e-09)
-    assert sec_system.atom_labels[3] == 'Cl'
-    assert sec_system.atom_positions[4][2].magnitude == approx(1.01078267e-09)
+    sec_system = archive.run[0].system[0]
+    assert sec_system.atoms.lattice_vectors[2][2].magnitude == approx(2.0108734e-09)
+    assert sec_system.atoms.labels[3] == 'Cl'
+    assert sec_system.atoms.positions[4][2].magnitude == approx(1.01078267e-09)
 
-    sec_scc = sec_run.section_single_configuration_calculation[0]
-    assert sec_scc.energy_total.magnitude == approx(-2.24380887e-16)
+    sec_scc = sec_run.calculation[0]
+    assert sec_scc.energy.total.value.magnitude == approx(-2.24380887e-16)
 
 
 def test_1(parser):
@@ -53,7 +53,7 @@ def test_1(parser):
 
     parser.parse('tests/data/test08/ethene_relax.out', archive, None)
 
-    sec_sccs = archive.section_run[0].section_single_configuration_calculation
+    sec_sccs = archive.run[0].calculation
     assert len(sec_sccs) == 33
-    assert sec_sccs[8].energy_total.magnitude == approx(-5.95750082e-17)
-    assert sec_sccs[2].atom_forces[5][1].magnitude == approx(-6.68217323e-10)
+    assert sec_sccs[8].energy.total.value.magnitude == approx(-5.95750082e-17)
+    assert sec_sccs[2].forces.total.value[5][1].magnitude == approx(-6.68217323e-10)
